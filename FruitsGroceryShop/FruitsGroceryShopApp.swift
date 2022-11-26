32e16ff53e0fdef87f5df7e26443b5fd73b21eeb
nikita <nikitin.ninise@gmail.com>
2022-11-26 05:34:24 +0200

    Initial Commit

---
+++ FruitsGroceryShop/FruitsGroceryShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsGroceryShopApp.swift
+//  FruitsGroceryShop
+//
+//  Created by Nikita on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsGroceryShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
